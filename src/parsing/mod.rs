b4a3c32aae109c9cf8b11c2cb91ce59da719499b
Arison <arison646@gmail.com>
2025-05-02 12:55:32 +0200

    attempting to add iterator for Trait Node

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -7,51 +7,51 @@ use token::TokenKind;
 struct Exp {}
 
 impl Node for Exp {
-    fn get_production_rules() -> Vec<ProductionRule> {
+    fn get_production_rules(&self) -> Vec<ProductionRule> {
         vec![ProductionRule::Token(TokenKind::IntegerLiteral)]
     }
+}
+
+struct Statement {}
+
+impl Node for Statement {
+    fn get_production_rules(&self) -> Vec<ProductionRule> {
+        vec![
+            ProductionRule::Token(TokenKind::ReturnKeyword),
+            ProductionRule::Node(Box::new(Exp {})),
+            ProductionRule::Token(TokenKind::Semicolon),
+        ]
+    }
+}
+
+struct Function {}
+
+impl Node for Function {
+    fn get_production_rules(&self) -> Vec<ProductionRule> {
+        vec![
+            ProductionRule::Token(TokenKind::Int),
+            ProductionRule::Token(TokenKind::Identifier),
+            ProductionRule::Token(TokenKind::OpenParen),
+            ProductionRule::Token(TokenKind::CloseParen),
+            ProductionRule::Token(TokenKind::OpenBrace),
+            ProductionRule::Node(Box::new(Statement {})),
+            ProductionRule::Token(TokenKind::CloseBrace),
+        ]
+    }
+}
+
+struct Program {
     
 }
 
-struct Statement {
-
-}
-
-impl Node for Statement {
-    fn get_production_rules() -> Vec<ProductionRule> {
-        vec![
-        ProductionRule::Token(TokenKind::ReturnKeyword),
-        ProductionRule::Node(Exp {}),
-        ProductionRule::Token(TokenKind::Semicolon)
-    ]
+impl Node for Program {
+    fn get_production_rules(&self) -> Vec<ProductionRule> {
+        vec![ProductionRule::Node(Box::new(Function {}))]
     }
     
 }
 
+fn parse() {
 
-fn parse(){
-
-    // defining grammar
-    let exp = Node::new(vec![ProductionRule::Token(TokenKind::IntegerLiteral)]);
-    let statement = Node::new(vec![
-        ProductionRule::Token(TokenKind::ReturnKeyword),
-        ProductionRule::Node(exp),
-        ProductionRule::Token(TokenKind::Semicolon)
-    ]);
-    let function = Node::new(vec![
-        ProductionRule::Token(TokenKind::Int),
-        ProductionRule::Token(TokenKind::Identifier),
-        ProductionRule::Token(TokenKind::OpenParen),
-        ProductionRule::Token(TokenKind::CloseParen),
-        ProductionRule::Token(TokenKind::OpenBrace),
-        ProductionRule::Node(statement),
-        ProductionRule::Token(TokenKind::CloseBrace)
-    ]);
-    let program = Node::new(vec![ProductionRule::Node(function)]);
-
-
-    
 
 }
-
-
--- src/parsing/nodes.rs
+++ src/parsing/nodes.rs
@@ -1,12 +1,60 @@
 use crate::lexing::token::TokenKind;
 
+// BUG: fix this lol
+
 pub enum ProductionRule {
     Token(TokenKind),
-    Node(Node),
+    Node(Box<dyn Node>),
 }
+
 
 pub trait Node {
     //production_rules: Vec<ProductionRule>,
-    fn get_production_rules() -> Vec<ProductionRule>;
+    fn get_production_rules(&self) -> Vec<ProductionRule>;
+    // Returns an iterator for Node 
+    fn iter_rules(&self) -> NodeIter<Self> 
+    where 
+        Self : Sized,
+    {
+        NodeIter::new(self)
+    }
+}
+
+
+// Wrapper Struct to implement iterator on any strcut that implments node
+pub struct NodeIter <'a, T: Node> {
+    node: &'a T,
+    rules: Vec<ProductionRule>,
+    index: usize,
+}
+
+impl <'a, T:Node> NodeIter<'a,T> {
+    pub fn new(node: &'a T) -> Self {
+        Self {node , rules : node.get_production_rules(), index : 0}
+    }
+}
+
+impl<'a, T:Node> Iterator for NodeIter<'a, T > {
+    // This will return a copy but since it's just an enum it shouldn't be an issue 
+    type Item = TokenKind;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.rules.len() == 0 {
+            return None;
+        }
+        let rule = &self.rules[0];
+
+        match rule {
+            ProductionRule::Token(t) => {
+                self.rules.pop();
+                return Some(*t)
+            },
+            ProductionRule::Node(n) => {
+
+            }
+        }
+
+        None
+    }
 }
 
